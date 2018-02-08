3495eacf4ce4a090976f72f9333f108493977aa1
tpetersheim <travispetersheim@gmail.com>
2018-02-08 13:27:23 -0600

    Initial commit

--- DotNetCLISearch/Program.cs
+++ DotNetCLISearch/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNetCLISearch
 {
@@ -10,6 +10,106 @@ namespace DotNetCLISearch
     {
         static void Main(string[] args)
         {
+            var app = new CommandLineApplication(false);
+            app.HelpOption("-h | --help");
+            app.Command("search", (searchCommand) =>
+            {
+                searchCommand.Description = "Search through files for a term";
+                var searchArg = searchCommand.Argument("search_term", "The term to search by.", false);
+
+
+                var searchDirectory = searchCommand.Option("-d | --directory <directory>",
+                    "The directory to search through. Defaults to the current directory.", CommandOptionType.SingleValue);
+                var caseInsensitive = searchCommand.Option("-i | --case-insensitive", "Search without case sensitivity.", CommandOptionType.NoValue);
+                var searchRecursively = searchCommand.Option("-r | --recursive", "Search recursively into sub-folders.", CommandOptionType.NoValue);
+                searchCommand.HelpOption("-h | --help");
+
+                searchCommand.OnExecute(() =>
+                {
+                    if (searchArg.Values.Count == 0)
+                    {
+                        Console.WriteLine("Search term missing. Aborting search.");
+                        searchCommand.ShowHelp();
+                        return -1;
+                    }
+
+                    var searchDir = GetSearchDirectory(searchDirectory);
+                    if (string.IsNullOrEmpty(searchDir))
+                    {
+                        Console.WriteLine("Invalid search directory. Aborting search.");
+                        return -1;
+                    }
+
+                    Console.WriteLine($"Searching for {searchArg.Value}");
+                    var sw = Stopwatch.StartNew();
+                    var resultCount = TextSearch.Search(searchArg.Value, searchDir, caseInsensitive.HasValue(), searchRecursively.HasValue());
+                    Console.WriteLine($"Found {resultCount} results in {sw.ElapsedMilliseconds} ms");
+
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+
+                    return 0;
+                });
+            });
+
+            app.Command("gentestfiles", (genTestFilesCommand) =>
+            {
+                genTestFilesCommand.Description = "Generate test files and folders in current directory. Search terms are \"test\" and \"Test\".";
+                genTestFilesCommand.HelpOption("-h | --help");
+                genTestFilesCommand.OnExecute(() =>
+                {
+                    const int numDirectoriesBreadth = 200;
+                    const int numDirectoriesDepth = 30;
+                    string testDirPath = $"{Environment.CurrentDirectory}\\generated_test_files";
+                    Directory.CreateDirectory(testDirPath);
+                    for (int i = 0; i < numDirectoriesBreadth; i++)
+                    {
+                        var path = $"{testDirPath}\\folder{i}";
+                        Directory.CreateDirectory(path);
+                        var subpath = $"{path}\\0";
+                        Directory.CreateDirectory(subpath);
+                        for (int j = 1; j < numDirectoriesDepth; j++)
+                        {
+                            subpath += $"\\{j}";
+                            Directory.CreateDirectory(subpath);
+                            File.WriteAllText($"{subpath}\\no-test-file.txt", String.Concat(Enumerable.Repeat(new String('b', 100) + "\r\n", 2500)));
+                            if (j == numDirectoriesDepth - 1)
+                            {
+                                string test = i % 2 == 0 ? "test" : "Test";
+                                File.WriteAllText($"{subpath}\\test-file.txt", String.Concat(Enumerable.Repeat(new String('a', 100) + "\r\n", 2500)) + test);
+                            }
+                        }
+                    }
+
+                    return 0;
+                });
+            });
+
+            try
+            {
+                app.Execute(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Search error: " + e.ToString());
+                app.ShowHelp();
+            }
+        }
+
+        private static string GetSearchDirectory(CommandOption searchDirectory)
+        {
+            string searchDir = null;
+
+            if (!searchDirectory.HasValue()) // No value. Default to current directory
+            {
+                searchDir = Environment.CurrentDirectory;
+            }
+            else if (Directory.Exists(searchDirectory.Value())) // Validate if directory given
+            {
+                searchDir = searchDirectory.Value();
+            }
+
+            return searchDir;
         }
     }
 }
---
+++ DotNetCLISearch/TextSearch.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DotNetCLISearch
+{
+    static class TextSearch
+    {
+        /// <summary>
+        /// Searches a given directory for a given term.  Prints results to console
+        /// </summary>
+        /// <param name="searchTerm"></param>
+        /// <param name="searchDirectory"></param>
+        /// <param name="caseInsensitive"></param>
+        /// <param name="searchRecursively"></param>
+        /// <returns>The number of search results</returns>
+        public static int Search(string searchTerm, string searchDirectory, Boolean caseInsensitive, Boolean searchRecursively)
+        {
+            var resultCount = 0;
+
+            if (caseInsensitive)
+            {
+                searchTerm = searchTerm.ToLower();
+            }
+
+            IEnumerable<string> filePaths = Enumerable.Empty<string>();
+            try
+            {
+                filePaths = EnumerateFiles(searchDirectory, searchRecursively);
+
+                foreach (var filePath in filePaths)
+                {
+                    try
+                    {
+                        var sw = Stopwatch.StartNew();
+                        var matches = SearchFile(filePath, searchTerm, caseInsensitive);
+                        resultCount += matches.Count();
+                        foreach (var match in matches)
+                        {
+                            var filePathLessBase = filePath.Replace(searchDirectory + "\\", "");
+                            var line = match.Value.Trim();
+                            if (line.Length > 100) {
+                                line = line.Substring(0, 100) + "...";
+                            }
+                            Console.WriteLine($"\"{searchTerm}\" found. Time {sw.ElapsedMilliseconds} ms. {filePathLessBase}:{match.Key}  {line}");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error searching file. {e.Message}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            Console.WriteLine($"Searched {filePaths.Count()} files");
+
+            return resultCount;
+        }
+
+        private static Dictionary<int, string> SearchFile(string filePath, string searchTerm, Boolean caseInsensitive)
+        {
+            var matches = new Dictionary<int, string>();
+
+            using (StreamReader stream = File.OpenText(filePath))
+            {
+                string line;
+                int i = 0;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    i++;
+                    var comparison = caseInsensitive ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+                    if (line.IndexOf(searchTerm, comparison) > -1)
+                    {
+                        matches.Add(i, line);
+                    }
+                }
+            }
+
+            return matches;
+        }
+
+        /**
+         * Directory.EnumerateFiles fails when it can't read a file/folder
+         */
+        private static IEnumerable<string> EnumerateFiles(string folderPath, Boolean enumerateRecursively)
+        {
+            var files = getFiles(folderPath);
+
+            for (int i = 0; i < files.Count(); i++)
+            {
+                yield return files.ElementAt(i);
+            }
+
+            if (enumerateRecursively)
+            {
+                var directories = getDirectories(folderPath);
+
+                for (int i = 0; i < directories.Count(); i++)
+                {
+                    foreach (var file in EnumerateFiles(directories.ElementAt(i), true))
+                    {
+                        yield return file;
+                    }
+                }
+            }
+        }
+
+        private static string[] getFiles(string folderPath)
+        {
+            var files = new string[0];
+
+            try
+            {
+                files = Directory.GetFiles(folderPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting files. {e.Message}");
+            }
+
+            return files;
+        }
+
+        private static string[] getDirectories(string folderPath)
+        {
+            var directories = new string[0];
+
+            try
+            {
+                directories = Directory.GetDirectories(folderPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting directories. {e.Message}");
+            }
+
+            return directories;
+        }
+    }
+}
